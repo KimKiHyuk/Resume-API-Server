621334cd51d75e08cac4e2d4bc55256ee8cde164
key Kim <keyhyuk.kim@gmail.com>
2019-10-25 22:33:55 +0900

    Change controller name

---
+++ APIServer/Controllers/ResumeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace APIServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ResumeController : ControllerBase
+    {
+        public ResumeController()
+        {
+        }
+
+
+        [HttpGet]
+        public void Get()
+        {
+        }
+    }
+}
