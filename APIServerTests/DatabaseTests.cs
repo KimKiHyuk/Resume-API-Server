c4526c8d594063a7fbc9a96daeba28e988972682
key Kim <keyhyuk.kim@gmail.com>
2019-10-25 22:27:30 +0900

    Expand test case

---
+++ APIServerTests/DatabaseTests.cs
@@ -0,0 +1,15 @@
+using Xunit;
+using System;
+namespace APIServerTests
+{
+    public class DatabaseTests
+    {
+        [Fact]
+        public void Database_Test()
+        {
+            // assign
+            // act
+            // assert
+        }
+    }
+}
---
+++ APIServerTests/HttpTests.cs
@@ -0,0 +1,15 @@
+using Xunit;
+using System;
+namespace APIServerTests
+{
+    public class HttpTests
+    {
+        [Fact]
+        public void HTTP_Test()
+        {
+            // assign
+            // act
+            // assert
+        }
+    }
+}
